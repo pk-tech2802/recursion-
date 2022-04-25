8f12d94db77bbfb8544bb63501081b37fc4e3070
pk-tech2802 <75025527+pk-tech2802@users.noreply.github.com>
2022-04-26 01:41:08 +0530

    Create all_valid_bracket_seq.cpp

---
+++ all_valid_bracket_seq.cpp
@@ -0,0 +1,37 @@
+// vvimp :- make the choise diagram then code pasrt is cakewack
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void sol(string op, int o, int c)
+{
+    if (o == 0 && c == 0)
+    {   
+        // if no unused opening and closing brackets remaining then simply print ans 
+        cout << op << "\n";
+        return;
+    }
+    string op1 = op, op2 = op;
+
+    if (o - 1 >= 0)
+    {   
+        // if open unued remaining u can always use it 
+        op1.push_back('(');
+        sol(op1, o - 1, c);
+    }
+
+    if (c - 1 >= 0 && o < c)
+    {   
+        // to use closed unused we have to ensure open unused is strictly less than close unused
+        // that is open used greater than cloed used 
+        op2.push_back(')');
+        sol(op2, o, c - 1);
+    }
+    return;
+}
+
+int main()
+{
+    sol("", 2, 2);
+    return 0;
+}
